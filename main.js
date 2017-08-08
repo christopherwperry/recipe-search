2fc82d45241e18979d829c7f0b0d29d278c5f863
christopherwperry <christopherwperry@gmail.com>
2017-08-08 11:00:28 -0400

    built wireframe display

---
+++ main.js
@@ -0,0 +1,19 @@
+
+fetch('https://www.recipepuppy.com/api/')
+  .then(function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+    });
+
+
+    /*
+
+
+    response.json().then(function(data) {
+
+
+    */// variable for search term and concat to end of url
