2d429c83050430dcfcc2e6ba99267b5bff3a8fa0
christopherwperry <christopherwperry@gmail.com>
2017-08-08 15:00:23 -0400

    styled results page

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let button = document.getElementById("searchButton");
 console.log(button);
 
-let proxy = 'http://recipepuppyproxy.herokuapp.com/api/?q=';
+let proxy = 'http://recipepuppyproxy.herokuapp.com/api/?i=';
 
 button.addEventListener("click", function(){
   let search_term = document.getElementById("top_box").value;
@@ -15,16 +15,19 @@ button.addEventListener("click", function(){
           return;
         }
       response.json().then(function(data) {
+        console.log(data.results[1].thumbnail)
         for(let i = 0; i < data.results.length; i++){
           let thumbnail = data.results[i].thumbnail;
           let title = data.results[i].title;
           let link = data.results[i].href;
+          let ingred = data.results[i].ingredients;
           let new_box = document.createElement("div");
           new_box.setAttribute("class", "box");
           let template = `
-          <img src="${thumbnail}" alt=""http://via.placeholder.com/150x150"">
-          <p>Title: ${title}</p>
-          <p>Link: <a href="${link}">Recipe</a></p>
+          <img src="${thumbnail}">
+          <p>${title}</p>
+          <p>Ingredients: ${ingred}</p>
+          <p><a href="${link}">Recipe</a></p>
           `
           new_box.innerHTML = template
           let box = document.querySelector(".content-boxes");
