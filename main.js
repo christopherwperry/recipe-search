697341ee4632fd310beda40d3e7c9d6e7e836eb4
christopherwperry <christopherwperry@gmail.com>
2017-08-08 12:58:34 -0400

    pulled data and pushed into new div for each search result

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
-fetch('https://www.recipepuppy.com/api/')
-  .then(function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
+let button = document.getElementById("searchButton");
+console.log(button);
 
-    });
+let proxy = 'http://recipepuppyproxy.herokuapp.com/api/?q=';
 
-    /*
-
-
-    response.json().then(function(data) {
-
-
-    */// variable for search term and concat to end of url
-
-    //for each item we need a thumbnail, title, and link to href
+button.addEventListener("click", function(){
+  let search_term = document.getElementById("top_box").value;
+  let searchUrl = proxy + search_term;
+  console.log(searchUrl);
+  fetch(searchUrl)
+    .then(function(response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+      response.json().then(function(data) {
+        for(let i = 0; i < data.results.length; i++){
+          let thumbnail = data.results[i].thumbnail;
+          let title = data.results[i].title;
+          let link = data.results[i].href;
+          let new_box = document.createElement("div");
+          new_box.setAttribute("class", "box");
+          let template = `
+          <img src="${thumbnail}">
+          <p>Title: ${title}</p>
+          <p>Link: ${link}</p>
+          `
+          new_box.innerHTML = template
+          let box = document.querySelector(".content-boxes");
+          box.appendChild(new_box);
+        }
+      });
+    })
+});
